d1fd1a49d3225983dbc0f747d79b9182370471a7
jeremiafourie <jeremia@exequtech.com>
2025-05-10 11:41:58 +0200

    whatsapp service update

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -4,9 +4,17 @@
  * @param {string} to — E.164 phone (e.g. "+1555…")
  * @param {string} text — message body
  */
-async function sendWhatsApp(to, text) {
-  console.log("TODO: Sending WhatsApp")
-  return
+async function sendWhatsApp(text, to) {
+  return await fetch(`${process.env.UAZAPI_BASEURL}/send/text`, {
+    method: 'POST',
+    headers: {'token': process.env.UAZAPI_KEY, 'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      number: to,
+      text
+    })
+  }).then(response => response.json())
+  .catch(err => console.error(`Failed to send whatsapp (to: ${to}, text: ${text}):\n`, err));
 }
 
 module.exports = { sendWhatsApp };
+
